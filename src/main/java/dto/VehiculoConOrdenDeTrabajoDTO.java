e97e3d142814bd4adb999cd2c4a20bd72c1c4a12
redvladimirogaston <redvladimirogaston@gmail.com>
2020-11-02 12:28:09 -0300

    add
    add clientedto clientesdaoimpl & ordentrabajodto & ordentrabajodaoimpl &
    vehiculoconordendetrabajodto

--- src/main/java/dto/VehiculoConOrdenDeTrabajoDTO.java
+++ src/main/java/dto/VehiculoConOrdenDeTrabajoDTO.java
@@ -2,4 +2,76 @@ package dto;
 
 public class VehiculoConOrdenDeTrabajoDTO {
 
+	private Integer id;
+	
+	private Integer idFichaTecnica;
+	
+	private Integer idCliente;
+	
+	private Integer kilometrajeGarantia;
+	
+	private String aseguradora;
+	
+	private Integer nroPolizaSeguro;
+	
+	private String patente;
+
+	public VehiculoConOrdenDeTrabajoDTO() {}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getIdFichaTecnica() {
+		return idFichaTecnica;
+	}
+
+	public void setIdFichaTecnica(Integer idFichaTecnica) {
+		this.idFichaTecnica = idFichaTecnica;
+	}
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public Integer getKilometrajeGarantia() {
+		return kilometrajeGarantia;
+	}
+
+	public void setKilometrajeGarantia(Integer kilometrajeGarantia) {
+		this.kilometrajeGarantia = kilometrajeGarantia;
+	}
+
+	public String getAseguradora() {
+		return aseguradora;
+	}
+
+	public void setAseguradora(String aseguradora) {
+		this.aseguradora = aseguradora;
+	}
+
+	public Integer getNroPolizaSeguro() {
+		return nroPolizaSeguro;
+	}
+
+	public void setNroPolizaSeguro(Integer nroPolizaSeguro) {
+		this.nroPolizaSeguro = nroPolizaSeguro;
+	}
+
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
 }
