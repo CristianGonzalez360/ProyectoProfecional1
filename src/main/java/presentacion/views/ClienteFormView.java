06ec0b5f075fa40d0195b0885282e1d7f9003d68
Cristian Gonzalez <goonzalez_cristian34@hotmail.com>
2020-11-04 13:33:09 -0300

    funcion boton cancelar en clienteForm

--- src/main/java/presentacion/views/ClienteFormView.java
+++ src/main/java/presentacion/views/ClienteFormView.java
@@ -9,7 +9,6 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import dto.AltaClienteDTO;
 import dto.ClienteDTO;
 import dto.DatosPersonalesDTO;
 
