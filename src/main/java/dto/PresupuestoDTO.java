b8786d59e0e0826c3c4c7cd608591f15cd3c2486
MarcoYu <marco.yu1995@gmail.com>
2020-11-04 19:28:21 -0300

    add DTO presupuesto, repuesto, repuestoplanificado y trabajopanificado

--- src/main/java/dto/PresupuestoDTO.java
+++ src/main/java/dto/PresupuestoDTO.java
@@ -1,5 +1,107 @@
 package dto;
 
-public class PresupuestoDTO {
+import java.util.Date;
 
+public class PresupuestoDTO {
+	  private Integer idPresupuesto;
+	  private Integer idOT;
+	  private Integer idUsuAltaPresu;
+	  private Integer idUsuCierrePresu;
+	  private Integer idUsuRegPago;
+	  private Integer idPago;
+	  private Date fechaAltaPresu;
+	  private String comentarioAltaPresu;
+	  private Date fechaCierrePresu;
+	  private String comentarioCierrePresu;
+	  private Date fechaAprobacion;
+	  private Date fechaRechazo;
+	  
+	  
+	  public PresupuestoDTO () {
+		  
+	  }
+
+	public Integer getIdPresupuesto() {
+		return idPresupuesto;
+	}
+	public void setIdPresupuesto(Integer idPresupuesto) {
+		this.idPresupuesto = idPresupuesto;
+	}
+	public Integer getIdOT() {
+		return idOT;
+	}
+	public void setIdOT(Integer idOT) {
+		this.idOT = idOT;
+	}
+	public Integer getIdUsuAltaPresu() {
+		return idUsuAltaPresu;
+	}
+	public void setIdUsuAltaPresu(Integer idUsuAltaPresu) {
+		this.idUsuAltaPresu = idUsuAltaPresu;
+	}
+	public Integer getIdUsuCierrePresu() {
+		return idUsuCierrePresu;
+	}
+	public void setIdUsuCierrePresu(Integer idUsuCierrePresu) {
+		this.idUsuCierrePresu = idUsuCierrePresu;
+	}
+	public Integer getIdUsuRegPago() {
+		return idUsuRegPago;
+	}
+	public void setIdUsuRegPago(Integer idUsuRegPago) {
+		this.idUsuRegPago = idUsuRegPago;
+	}
+	public Integer getIdPago() {
+		return idPago;
+	}
+	public void setIdPago(Integer idPago) {
+		this.idPago = idPago;
+	}
+	public Date getFechaAltaPresu() {
+		return fechaAltaPresu;
+	}
+	public void setFechaAltaPresu(Date fechaAltaPresu) {
+		this.fechaAltaPresu = fechaAltaPresu;
+	}
+	public String getComentarioAltaPresu() {
+		return comentarioAltaPresu;
+	}
+	public void setComentarioAltaPresu(String comentarioAltaPresu) {
+		this.comentarioAltaPresu = comentarioAltaPresu;
+	}
+	public Date getFechaCierrePresu() {
+		return fechaCierrePresu;
+	}
+	public void setFechaCierrePresu(Date fechaCierrePresu) {
+		this.fechaCierrePresu = fechaCierrePresu;
+	}
+	public String getComentarioCierrePresu() {
+		return comentarioCierrePresu;
+	}
+	public void setComentarioCierrePresu(String comentarioCierrePresu) {
+		this.comentarioCierrePresu = comentarioCierrePresu;
+	}
+	public Date getFechaAprobacion() {
+		return fechaAprobacion;
+	}
+	public void setFechaAprobacion(Date fechaAprobacion) {
+		this.fechaAprobacion = fechaAprobacion;
+	}
+	public Date getFechaRechazo() {
+		return fechaRechazo;
+	}
+	public void setFechaRechazo(Date fechaRechazo) {
+		this.fechaRechazo = fechaRechazo;
+	}
+	
+	@Override
+	public String toString() {
+		return "PresupuestoDTO [idPresupuesto=" + idPresupuesto + ", idOT=" + idOT + ", idUsuAltaPresu="
+				+ idUsuAltaPresu + ", idUsuCierrePresu=" + idUsuCierrePresu + ", idUsuRegPago=" + idUsuRegPago
+				+ ", idPago=" + idPago + ", fechaAltaPresu=" + fechaAltaPresu + ", comentarioAltaPresu="
+				+ comentarioAltaPresu + ", fechaCierrePresu=" + fechaCierrePresu + ", comentarioCierrePresu="
+				+ comentarioCierrePresu + ", fechaAprobacion=" + fechaAprobacion + ", fechaRechazo=" + fechaRechazo
+				+ "]";
+	}
+	
 }
