79f1a128cc52499d3ea4f7e83f950f148f71bca8
Cristian Gonzalez <goonzalez_cristian34@hotmail.com>
2020-11-06 00:06:24 -0300

    Guardar presupuesto con errores

--- src/main/java/business_logic/PresupuestosController.java
+++ src/main/java/business_logic/PresupuestosController.java
@@ -3,6 +3,8 @@ package business_logic;
 import java.util.Date;
 
 import dto.PresupuestoDTO;
+import dto.RepuestoPlanificadoDTO;
+import dto.TrabajoPresupuestadoDTO;
 import repositories.PresupuestosDao;
 import repositories.RepuestosPlanificadosDao;
 import repositories.TrabajosPresupuestadosDao;
@@ -12,17 +14,26 @@ public class PresupuestosController {
 	
 	private PresupuestosDao Pdao;
 	private TrabajosPresupuestadosDao TPDao;
-	private RepuestosPlanificadosDao RPDap;
+	private RepuestosPlanificadosDao RPDao;
 	
 
 	public PresupuestosController(PresupuestosDao presupuestosDao, TrabajosPresupuestadosDao trabajosPresupuestadosDao, RepuestosPlanificadosDao repuestosPlanificadosDao) {
 		this.Pdao = presupuestosDao;
 		this.TPDao = trabajosPresupuestadosDao;
-		this.RPDap = repuestosPlanificadosDao;
+		this.RPDao = repuestosPlanificadosDao;
 	}
 
 	public void save(PresupuestoDTO presupuesto) {
 		presupuesto.setIdUsuAltaPresu(SessionServiceImpl.getInstance().getActiveSession().getIdUsuario());
 		presupuesto.setFechaAltaPresu(new Date());
+		Pdao.insert(presupuesto);
+		for (TrabajoPresupuestadoDTO t : presupuesto.getTrabajos()) {
+			t.setIdPresupuesto(presupuesto.getIdPresupuesto());//TODO Esta mal, como obtengo el id?
+			TPDao.insert(t);
+		}
+		for (RepuestoPlanificadoDTO r : presupuesto.getRepuestos()) {
+			r.setIdPresu(presupuesto.getIdPresupuesto());//TODO Esta mal, como obtengo el id?
+			RPDao.insert(r);
+		}
 	}
 }
