4efd4f435e0e41dba344c5fad353e2b4eb08dcb8
pabloabregu <pablo.abregu@hotmail.com>
2020-11-11 19:12:23 -0300

    update view Entrega de Vehiculo

---
+++ src/main/java/presentacion/views/supervisor/EntregaVehiculosPanelView.java
@@ -0,0 +1,69 @@
+package presentacion.views.supervisor;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
+public class EntregaVehiculosPanelView extends JPanel {
+
+	private static final long serialVersionUID = -5623725856065117794L;
+	
+	private static final String[] COLUMNAS_ENTREGAS = {"DNI CLIENTE", "MARCA DE AUTO", "MODELO DE AUTO", "COLOR", "PATENTE"};
+	
+	private JLabel lblDniCliente;
+	private JTextField textDniCliente;
+	private JButton btnBuscar;
+	
+	private DefaultTableModel tableModelEntregas;
+	private JTable tableEntregas;
+	
+	private JButton btnRegistrarEntrega;
+
+	public EntregaVehiculosPanelView() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelSuperior = new JPanel();
+		panelSuperior.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		FlowLayout flowLayout = (FlowLayout) panelSuperior.getLayout();
+		flowLayout.setHgap(20);
+		add(panelSuperior, BorderLayout.NORTH);
+
+		lblDniCliente = new JLabel("Cliente DNI");
+		panelSuperior.add(lblDniCliente);
+
+		textDniCliente = new JTextField();
+		textDniCliente.setHorizontalAlignment(SwingConstants.CENTER);
+		panelSuperior.add(textDniCliente);
+		textDniCliente.setColumns(10);
+
+		btnBuscar = new JButton("Buscar");
+		panelSuperior.add(btnBuscar);
+
+		JPanel panelCentro = new JPanel();
+		add(panelCentro, BorderLayout.CENTER);
+
+		JScrollPane scrollPaneEntregas = new JScrollPane();
+		add(scrollPaneEntregas, BorderLayout.CENTER);
+		tableModelEntregas = new DefaultTableModel(null, COLUMNAS_ENTREGAS);
+		tableEntregas = new JTable(tableModelEntregas);
+		scrollPaneEntregas.setViewportView(tableEntregas);
+		
+		JPanel panelInferior = new JPanel();
+		add(panelInferior, BorderLayout.SOUTH);
+
+		btnRegistrarEntrega = new JButton("Registrar turno");
+		panelInferior.add(btnRegistrarEntrega);
+	}
+	
+}
