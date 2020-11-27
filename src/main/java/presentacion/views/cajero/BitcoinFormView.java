7de9b6d67df866106608489bd25d704b9d796593
pabloabregu <pablo.abregu@hotmail.com>
2020-11-26 23:22:20 -0300

    se agrego titulo a ventana de pago con bitcoin

--- src/main/java/presentacion/views/cajero/BitcoinFormView.java
+++ src/main/java/presentacion/views/cajero/BitcoinFormView.java
@@ -10,21 +10,11 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
-
-import dto.AltaClienteDTO;
-import dto.ClienteDTO;
-import dto.DatosPersonalesDTO;
-import dto.TarjetaCreditoDTO;
-
 import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
 
 public class BitcoinFormView extends JDialog {
 
@@ -35,31 +25,33 @@ public class BitcoinFormView extends JDialog {
 	private JButton btnSalvar;
 
 	private BitcoinFormView() {
+		setTitle("Pago con Bitcoin");
 		setBounds(100, 100, 461, 579);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		setModal(true);
-		
 
 		JPanel panel = new JPanel();
 		contentPanel.add(panel);
 		panel.setLayout(null);
 
-		JLabel lblNumeroTarjeta = new JLabel("Escane el QR:");
-		lblNumeroTarjeta.setBounds(14, 3, 105, 14);
+		JLabel lblNumeroTarjeta = new JLabel("Escanear el siguiente Código QR");
+		lblNumeroTarjeta.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNumeroTarjeta.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNumeroTarjeta.setBounds(10, 11, 415, 14);
 		panel.add(lblNumeroTarjeta);
-		
+
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(73, 28, 316, 420);
+		panel_2.setBounds(48, 57, 350, 350);
 		panel.add(panel_2);
 
 		JPanel panel_1 = new JPanel();
 		contentPanel.add(panel_1, BorderLayout.SOUTH);
 
-		btnSalvar = new JButton("Confirmar recepcion");
+		btnSalvar = new JButton("Confirmar Recepción");
 		panel_1.add(btnSalvar);
 
 		JButton btnCancelar = new JButton("Cancelar");
@@ -72,20 +64,21 @@ public class BitcoinFormView extends JDialog {
 
 		setVisible(false);
 		clearData();
-		
+
 		Imagen Imagen = new Imagen();
 		panel_2.add(Imagen);
 		panel_2.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa");
-		lblNewLabel.setBounds(10, 472, 323, 14);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 472, 415, 14);
 		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("O copie esta direccion:");
-		lblNewLabel_1.setBounds(10, 454, 125, 14);
+
+		JLabel lblNewLabel_1 = new JLabel("O puede copiar la siguiente dirección:");
+		lblNewLabel_1.setBounds(10, 447, 415, 14);
 		panel.add(lblNewLabel_1);
 		panel_2.repaint();
-	
+
 	}
 
 	public static BitcoinFormView getInstance() {
@@ -96,30 +89,26 @@ public class BitcoinFormView extends JDialog {
 	}
 
 	public class Imagen extends javax.swing.JPanel {
-		 
-		/**
-		 * 
-		 */
 		private static final long serialVersionUID = -2207187888506087014L;
 
 		public Imagen() {
-		this.setSize(300, 400); 
+			this.setSize(350, 350);
 		}
-		 			 
+
 		public void paint(Graphics grafico) {
-		Dimension height = getSize();
-		ImageIcon Img = new ImageIcon(getClass().getResource("/icons/qr.png")); 
-		 
-		grafico.drawImage(Img.getImage(), 0, 0, height.width, height.height, null);
-		 
-		setOpaque(false);
-		super.paintComponent(grafico);
+			Dimension height = getSize();
+			ImageIcon Img = new ImageIcon(getClass().getResource("/icons/qr.png"));
+
+			grafico.drawImage(Img.getImage(), 0, 0, height.width, height.height, null);
+
+			setOpaque(false);
+			super.paintComponent(grafico);
 		}
-		}
+	}
 
 	public void setActionOnRegistrar(ActionListener listener) {
 		this.btnSalvar.addActionListener(listener);
-		
+
 	}
 
 	public void clearData() {
@@ -133,4 +122,5 @@ public class BitcoinFormView extends JDialog {
 
 	public void close() {
 		setVisible(false);
-	}}
+	}
+}
