c6003eca88159e6f8db4b684dd2a885f602edcf1
pabloabregu <pablo.abregu@hotmail.com>
2020-12-03 10:32:31 -0300

    formateo de codigo

--- src/main/java/presentacion/HistorialVentasPresenter.java
+++ src/main/java/presentacion/HistorialVentasPresenter.java
@@ -2,24 +2,8 @@
 package presentacion;
 
 import java.awt.event.ActionEvent;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Date;
 
-import javax.swing.JOptionPane;
-
-import business_logic.FacturasController;
 import business_logic.VentasVehiculosController;
-import business_logic.exceptions.ConflictException;
-import dto.TarjetaCreditoDTO;
-import dto.TarjetaDebitoDTO;
-import presentacion.views.cajero.BitcoinFormView;
-import presentacion.views.cajero.MercadoPagoFormView;
-import presentacion.views.cajero.PanelCobroCajeroView;
-import presentacion.views.cajero.TarjetaCreditoFormView;
-import presentacion.views.cajero.TarjetaDebitoFormView;
-import presentacion.views.utils.MessageDialog;
 import presentacion.views.vendedor.HistorialVentasView;
 import services.SessionServiceImpl;
 
@@ -27,32 +11,21 @@ public class HistorialVentasPresenter {
 
 	private HistorialVentasView view;
 	private VentasVehiculosController ventasVehiculosController;
-	
 
 	public HistorialVentasPresenter(VentasVehiculosController ventasVehiculosController) {
 
 		this.ventasVehiculosController = ventasVehiculosController;
 		this.view = HistorialVentasView.getInstance();
-		
 
 		this.view.setActionOnBuscar(a -> onCargar(a));
-
-		
 	}
-
-	
 
 	private void onCargar(ActionEvent a) {
-	
 		this.view.clear();
-		
+
 		Integer userID = SessionServiceImpl.getInstance().getActiveSession().getIdUsuario();
-		 
-		 this.view.cargarTabla(this.ventasVehiculosController.readByIdVendedor(userID,view.getVentaDesde(),view.getVentaHasta()));
-	
+		this.view.cargarTabla(
+				this.ventasVehiculosController.readByIdVendedor(userID, view.getVentaDesde(), view.getVentaHasta()));
 	}
-
-	
-
 
 }
--- src/main/java/presentacion/views/gerente/GerenteControllerView.java
+++ src/main/java/presentacion/views/gerente/GerenteControllerView.java
@@ -32,7 +32,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 public class GerenteControllerView extends JInternalFrame {
-
+	private static final long serialVersionUID = 1L;
 
 	private static final String[] COLUMNAS_TURNOS = new String[] { "NRO. TURNO", "NOMBRE DE CLIENTE", "DNI",
 			"FECHA DE ALTA", "FECHA PROGRAMADA", "TELEFONO", "EMAIL" };
