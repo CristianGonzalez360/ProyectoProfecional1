67a4d8a54bcd6fa8d8a28caa1ae01fda0ab3d8c3
Cristian Gonzalez <goonzalez_cristian34@hotmail.com>
2020-12-13 16:20:41 -0300

    add al vender un auto se registra un VehiculoConOrdenDeTrabajo

--- src/main/java/business_logic/GarantiasController.java
+++ src/main/java/business_logic/GarantiasController.java
@@ -1,6 +1,10 @@
 package business_logic;
 
+import java.util.Date;
+
 import dto.GarantiaVehiculoDTO;
+import dto.VehiculoDTO;
+import dto.taller.FichaTecnicaVehiculoDTO;
 import repositories.DaosFactory;
 
 public class GarantiasController {
@@ -14,5 +18,20 @@ public class GarantiasController {
 	
 	public GarantiaVehiculoDTO readByIdVehiculo(Integer id) {
 		return daos.makeGarantiasVehiculosDao().readByIdVehiculo(id);
+	}
+	
+	public boolean estaEnGarantia(Integer idVehiculo) {
+		boolean ret = true;
+		GarantiaVehiculoDTO garantia = readByIdVehiculo(idVehiculo);
+		Date fechaCaducidad = garantia.getFechaDeCaducidadDeLaGarantia();
+		if(fechaCaducidad.after(new Date())) {
+			ret = false;
+		} 
+		VehiculoDTO vehiculo = daos.makeVehiculoDao().readByID(idVehiculo);
+		FichaTecnicaVehiculoDTO ficha = daos.makeFichaTecnicaVehiculoDao().readByID(vehiculo.getIdFichaTecnica());
+		if(ficha.getKilometraje() > garantia.getKilometrajeGarantizado()-garantia.getKilometrajeInicialDelVehiculo()){
+			ret = false;
+		}
+		return ret;
 	}
 }
--- src/main/java/repositories/jdbc/VehiculosConOrdenDeTrabajoDaoImpl.java
+++ src/main/java/repositories/jdbc/VehiculosConOrdenDeTrabajoDaoImpl.java
@@ -10,7 +10,7 @@ import repositories.jdbc.utils.Mapper;
 public class VehiculosConOrdenDeTrabajoDaoImpl extends GenericJdbcDao<VehiculoConOrdenDeTrabajoDTO>
 		implements VehiculosConOrdenDeTrabajoDao {
 
-	private static final String insert = "INSERT INTO VehiculoConOrdenesDeTrabajo (idFichaTecnicaVehiculo,idCliente,kilometrajeGarantia,aseguradora,nroPolizaSeguro,patenteVehiculo) VALUES (?,?,?,?,?,?)";
+	private static final String insert = "INSERT INTO VehiculoConOrdenesDeTrabajo (idFichaTecnicaVehiculo,idCliente,kilometrajeGarantia,aseguradora,nroPolizaSeguro,patenteVehiculo,idVehiculo) VALUES (?,?,?,?,?,?,?)";
 
 	private static final String readAll = "SELECT * FROM VehiculoConOrdenesDeTrabajo";
 
@@ -33,7 +33,7 @@ public class VehiculosConOrdenDeTrabajoDaoImpl extends GenericJdbcDao<VehiculoCo
 	public boolean insert(VehiculoConOrdenDeTrabajoDTO entity) {
 		return getTemplate().query(insert).param(entity.getIdFichaTecnica()).param(entity.getIdCliente())
 				.param(entity.getKilometrajeGarantia()).param(entity.getAseguradora())
-				.param(entity.getNroPolizaSeguro()).param(entity.getPatente()).excecute();
+				.param(entity.getNroPolizaSeguro()).param(entity.getPatente()).param(entity.getIdVehiculo()).excecute();
 	}
 
 	@Override
@@ -78,6 +78,7 @@ public class VehiculosConOrdenDeTrabajoDaoImpl extends GenericJdbcDao<VehiculoCo
 				dto.setAseguradora((String) obj[4]);
 				dto.setNroPolizaSeguro((Integer) obj[5]);
 				dto.setPatente((String) obj[6]);
+				dto.setIdVehiculo((Integer) obj[7]);
 				return dto;
 			}
 		};
