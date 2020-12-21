d52dd26959de462478710309045a6f468171f801
MarcoYu <marco.yu1995@gmail.com>
2020-12-21 14:19:20 -0300

    fix seed db

--- src/main/java/services/DatabaseGraph.java
+++ src/main/java/services/DatabaseGraph.java
@@ -2,26 +2,11 @@ package services;
 
 import java.util.LinkedList;
 import java.util.List;
-
-import dto.CaracteristicaVehiculoDTO;
-import dto.ClienteDTO;
 import dto.CuentaDTO;
 import dto.DatosPersonalesDTO;
-import dto.GarantiaVehiculoDTO;
 import dto.MonedaDTO;
 import dto.SucursalDTO;
 import dto.UsuarioDTO;
-import dto.VehiculoDTO;
-import dto.VentaVehiculoDTO;
-import dto.taller.FacturaDTO;
-import dto.taller.FichaTecnicaVehiculoDTO;
-import dto.taller.OrdenDeTrabajoDTO;
-import dto.taller.PresupuestoDTO;
-import dto.taller.RepuestoDTO;
-import dto.taller.RepuestoPlanificadoDTO;
-import dto.taller.TrabajoPresupuestadoDTO;
-import dto.taller.TurnoDTO;
-import dto.taller.IngresoOrdenDeTrabajoDTO;
 
 public class DatabaseGraph {
 
@@ -31,56 +16,16 @@ public class DatabaseGraph {
 
 	private List<UsuarioDTO> usuarios;
 
-	private List<TurnoDTO> turnos;
-
-	private List<FichaTecnicaVehiculoDTO> fichaTecnicaVehiculos;
-
-	private List<OrdenDeTrabajoDTO> ordenesDeTrabajo;
-
-	private List<IngresoOrdenDeTrabajoDTO> vehiculosConOrdenDeTrabajo;
-
-	private List<ClienteDTO> clientes;
-
-	private List<RepuestoDTO> repuestos;
-
-	private List<PresupuestoDTO> presupuestos;
-
-	private List<TrabajoPresupuestadoDTO> trabajos;
-
-	private List<RepuestoPlanificadoDTO> repuestosPlanificados;
-
-	private List<FacturaDTO> facturas;
-
-	private List<VentaVehiculoDTO> ventaVehiculo;
-
-	private List<CaracteristicaVehiculoDTO> caracteristicaVehiculo;
-
-	private List<VehiculoDTO> vehiculos;
-
 	private List<MonedaDTO> monedas;
 
 	private List<SucursalDTO> sucursales;
-
-	private List<GarantiaVehiculoDTO> garantias;
 
 	public DatabaseGraph() {
 		cuentas = new LinkedList<>();
 		datosPersonales = new LinkedList<>();
 		usuarios = new LinkedList<>();
-		turnos = new LinkedList<>();
-		ordenesDeTrabajo = new LinkedList<>();
-		vehiculosConOrdenDeTrabajo = new LinkedList<>();
-		clientes = new LinkedList<>();
-		repuestos = new LinkedList<>();
-		presupuestos = new LinkedList<>();
-		trabajos = new LinkedList<>();
-		repuestosPlanificados = new LinkedList<>();
-		ventaVehiculo = new LinkedList<>();
-		caracteristicaVehiculo = new LinkedList<>();
-		vehiculos = new LinkedList<>();
 		monedas = new LinkedList<>();
 		sucursales = new LinkedList<>();
-		garantias = new LinkedList<>();
 	}
 
 	public List<CuentaDTO> getCuentas() {
@@ -107,110 +52,6 @@ public class DatabaseGraph {
 		this.usuarios = usuarios;
 	}
 
-	public List<TurnoDTO> getTurnos() {
-		return turnos;
-	}
-
-	public void setTurnos(List<TurnoDTO> turnos) {
-		this.turnos = turnos;
-	}
-
-	public List<FichaTecnicaVehiculoDTO> getFichaTecnicaVehiculos() {
-		return fichaTecnicaVehiculos;
-	}
-
-	public void setFichaTecnicaVehiculos(List<FichaTecnicaVehiculoDTO> fichaTecnicaVehiculos) {
-		this.fichaTecnicaVehiculos = fichaTecnicaVehiculos;
-	}
-
-	public List<OrdenDeTrabajoDTO> getOrdenesDeTrabajo() {
-		return ordenesDeTrabajo;
-	}
-
-	public void setOrdenesDeTrabajo(List<OrdenDeTrabajoDTO> ordenesDeTrabajo) {
-		this.ordenesDeTrabajo = ordenesDeTrabajo;
-	}
-
-	public List<IngresoOrdenDeTrabajoDTO> getVehiculosConOrdenDeTrabajo() {
-		return vehiculosConOrdenDeTrabajo;
-	}
-
-	public void setVehiculosConOrdenDeTrabajo(List<IngresoOrdenDeTrabajoDTO> vehiculosConOrdenDeTrabajo) {
-		this.vehiculosConOrdenDeTrabajo = vehiculosConOrdenDeTrabajo;
-	}
-
-	public List<ClienteDTO> getClientes() {
-		return clientes;
-	}
-
-	public void setClientes(List<ClienteDTO> clientes) {
-		this.clientes = clientes;
-	}
-
-	public List<RepuestoDTO> getRepuestos() {
-		return repuestos;
-	}
-
-	public void setRepuestos(List<RepuestoDTO> repuestos) {
-		this.repuestos = repuestos;
-	}
-
-	public List<PresupuestoDTO> getPresupuestos() {
-		return presupuestos;
-	}
-
-	public void setPresupuestos(List<PresupuestoDTO> presupuestos) {
-		this.presupuestos = presupuestos;
-	}
-
-	public List<TrabajoPresupuestadoDTO> getTrabajos() {
-		return trabajos;
-	}
-
-	public void setTrabajos(List<TrabajoPresupuestadoDTO> trabajos) {
-		this.trabajos = trabajos;
-	}
-
-	public List<RepuestoPlanificadoDTO> getRepuestosPlanificados() {
-		return repuestosPlanificados;
-	}
-
-	public void setRepuestosPlanificados(List<RepuestoPlanificadoDTO> repuestosPlanificados) {
-		this.repuestosPlanificados = repuestosPlanificados;
-	}
-
-	public List<FacturaDTO> getFacturas() {
-		return facturas;
-	}
-
-	public void setFacturas(List<FacturaDTO> facturas) {
-		this.facturas = facturas;
-	}
-
-	public List<VentaVehiculoDTO> getVentaVehiculo() {
-		return ventaVehiculo;
-	}
-
-	public void setVentaVehiculo(List<VentaVehiculoDTO> ventaVehiculo) {
-		this.ventaVehiculo = ventaVehiculo;
-	}
-
-	public List<CaracteristicaVehiculoDTO> getCaracteristicaVehiculo() {
-		return caracteristicaVehiculo;
-	}
-
-	public void setCaracteristicaVehiculo(List<CaracteristicaVehiculoDTO> caracteristicaVehiculo) {
-		this.caracteristicaVehiculo = caracteristicaVehiculo;
-	}
-
-	public List<VehiculoDTO> getVehiculos() {
-		return vehiculos;
-	}
-
-	public void setVehiculos(List<VehiculoDTO> vehiculos) {
-		this.vehiculos = vehiculos;
-	}
-
 	public List<MonedaDTO> getMonedas() {
 		return monedas;
 	}
@@ -227,11 +68,4 @@ public class DatabaseGraph {
 		this.sucursales = sucursales;
 	}
 
-	public List<GarantiaVehiculoDTO> getGarantias() {
-		return garantias;
-	}
-
-	public void setGarantias(List<GarantiaVehiculoDTO> garantias) {
-		this.garantias = garantias;
-	}
 }
--- src/main/java/services/DatabaseSeederServiceImpl.java
+++ src/main/java/services/DatabaseSeederServiceImpl.java
@@ -4,26 +4,11 @@ import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
-
-import dto.CaracteristicaVehiculoDTO;
-import dto.ClienteDTO;
 import dto.CuentaDTO;
 import dto.DatosPersonalesDTO;
-import dto.GarantiaVehiculoDTO;
 import dto.MonedaDTO;
 import dto.SucursalDTO;
 import dto.UsuarioDTO;
-import dto.VehiculoDTO;
-import dto.VentaVehiculoDTO;
-import dto.taller.FacturaDTO;
-import dto.taller.FichaTecnicaVehiculoDTO;
-import dto.taller.OrdenDeTrabajoDTO;
-import dto.taller.PresupuestoDTO;
-import dto.taller.RepuestoDTO;
-import dto.taller.RepuestoPlanificadoDTO;
-import dto.taller.TrabajoPresupuestadoDTO;
-import dto.taller.TurnoDTO;
-import dto.taller.IngresoOrdenDeTrabajoDTO;
 import repositories.DaosFactory;
 import javax.inject.Inject;
 import java.io.InputStream;
@@ -52,68 +37,12 @@ public class DatabaseSeederServiceImpl {
 			daos.makeUsuariosDao().insert(target);
 			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
 		}
-		for (TurnoDTO target : graph.getTurnos()) {
-			daos.makeTurnosDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (FichaTecnicaVehiculoDTO target : graph.getFichaTecnicaVehiculos()) {
-			daos.makeFichaTecnicaVehiculoDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (ClienteDTO target : graph.getClientes()) {
-			daos.makeClienteDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (IngresoOrdenDeTrabajoDTO target : graph.getVehiculosConOrdenDeTrabajo()) {
-			daos.makeVehiculoConOrdeDeTrabajoDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (OrdenDeTrabajoDTO target : graph.getOrdenesDeTrabajo()) {
-			daos.makeOrdenDeTrabajoDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (RepuestoDTO target : graph.getRepuestos()) {
-			daos.makeRepuestoDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (PresupuestoDTO target : graph.getPresupuestos()) {
-			daos.makePresupuestoDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (TrabajoPresupuestadoDTO target : graph.getTrabajos()) {
-			daos.makeTrabajosPlanificadosDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (RepuestoPlanificadoDTO target : graph.getRepuestosPlanificados()) {
-			daos.makeRepuestosPlanificadosDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (FacturaDTO target : graph.getFacturas()) {
-			daos.makeFacturasDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (CaracteristicaVehiculoDTO target : graph.getCaracteristicaVehiculo()) {
-			daos.makeCaracteristicasVehiculoDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
 		for (MonedaDTO target : graph.getMonedas()) {
 			daos.makeMonedasDao().insert(target);
 			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
 		}
 		for (SucursalDTO target : graph.getSucursales()) {
 			daos.makeSucursalesDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (VehiculoDTO target : graph.getVehiculos()) {
-			daos.makeVehiculoDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (VentaVehiculoDTO target : graph.getVentaVehiculo()) {
-			daos.makeVentaVehiculoDao().insert(target);
-			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
-		}
-		for (GarantiaVehiculoDTO target : graph.getGarantias()) {
-			daos.makeGarantiasVehiculosDao().insert(target);
 			LogManager.getLogger(this.getClass()).log(Level.INFO, "Seed database >>>>>> " + target.toString());
 		}
 	}
